d8ad01029bf059da440457c1970f445bf10f6061
Erik <erik.grundy@gmail.com>
2021-12-04 00:49:35 +1300

    Day 3 - whew, this one was fiddly!

---
+++ src/Day3.hs
@@ -0,0 +1,49 @@
+module Day3 where
+
+import Control.Applicative
+import Data.Bits
+import Data.Function
+import Data.List
+
+parse :: String -> [String]
+parse = lines
+
+part1 :: [String] -> Int
+part1 ns = gamma * epsilon
+  where
+    gamma = foldBits $ map moreCommon $ transpose ns
+    epsilon = foldBits $ map lessCommon $ transpose ns
+
+part2 :: [String] -> Int
+part2 ns = co2Scrubber * o2Generator
+  where
+    o2Generator = foldBits (findBest moreCommon ns)
+    co2Scrubber = foldBits (findBest lessCommon ns)
+
+foldBits :: String -> Int
+foldBits = foldl' f 0
+  where
+    f n '1' = shiftL n 1 .|. 1
+    f n '0' = shiftL n 1 .|. 0
+
+moreCommon :: [Char] -> Char
+moreCommon line =
+  if (length line - 1) `div` 2 < length (filter (== '1') line)
+    then '1'
+    else '0'
+
+lessCommon :: [Char] -> Char
+lessCommon line =
+  if (length line - 1) `div` 2 < length (filter (== '1') line)
+    then '0'
+    else '1'
+
+findBest :: ([Char] -> Char) -> [String] -> String
+findBest strategy = go 0
+  where
+    go _ [option] = option
+    go n options =
+      go (n + 1) $
+      filter
+        (\option -> (option !! n) == strategy (transpose options !! n))
+        options
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,9 +3,10 @@ import Control.Monad
 
 import qualified Day1 as D1
 import qualified Day2 as D2
+import qualified Day3 as D3
 
 main :: IO Counts
-main = runTestTT $ TestList [day1, day2]
+main = runTestTT $ TestList [day1, day2, day3]
 
 withInput :: FilePath -> (String -> a) -> (a -> Assertion) -> Assertion
 withInput file parser action = action . parser =<< readFile file
@@ -39,3 +40,29 @@ day2 =
       [ (D2.Forward, 5) , (D2.Down, 5) , (D2.Forward, 8)
       , (D2.Up, 3) , (D2.Down, 8) , (D2.Forward, 2) ]
     strEx = "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2"
+
+day3 :: Test
+day3 =
+  "day 3 tests" ~:
+  TestList
+    [ "part 1 example" ~: 198 ~=? D3.part1 example
+    , "part 2 example" ~: 230 ~=? D3.part2 example
+    , "real deal" ~: withInput "resources/3.txt" D3.parse $ \i -> do
+        4138664 @=? D3.part1 i
+        4273224 @=? D3.part2 i
+    ]
+  where
+    example =
+      [ "00100"
+      , "11110"
+      , "10110"
+      , "10111"
+      , "10101"
+      , "01111"
+      , "00111"
+      , "11100"
+      , "10000"
+      , "11001"
+      , "00010"
+      , "01010"
+      ]
