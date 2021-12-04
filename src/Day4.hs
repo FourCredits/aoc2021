82e5bd365cffa15466feb5507bf21bfea1ab43d4
Erik <erik.grundy@gmail.com>
2021-12-04 23:07:41 +1300

    Day 4 - Turns out, sometimes manual parsing is just easier!

---
+++ src/Day4.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE TupleSections #-}
+
+module Day4 where
+
+import Data.List
+
+type Board = [[(Int, Bool)]]
+
+mkBoard :: [[Int]] -> Board
+mkBoard = map (map (, False))
+
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf n [] = []
+chunksOf n as = take n as : chunksOf n (drop n as)
+
+parse :: String -> ([Int], [Board])
+parse s =
+  let (cNumbers:_:boardLines) = lines s
+      chosenNumbers = read $ '[' : cNumbers ++ "]"
+      boardsChunks = map (take 5) $ chunksOf 6 boardLines
+      parseLine l = read $ '[' : intercalate "," (words l) ++ "]"
+      boards = map (mkBoard . map parseLine) boardsChunks
+   in (chosenNumbers, boards)
+
+hasWon :: Board -> Bool
+hasWon board = any wholeLine board || any wholeLine (transpose board)
+  where
+    wholeLine = all snd
+
+update :: Int -> Board -> Board
+update drawnNumber = map (map f)
+  where
+    f (number, marked)
+      | number == drawnNumber = (number, True)
+      | otherwise = (number, marked)
+    
+calculateScore :: Board -> Int
+calculateScore board = sum (map fst $ filter (not . snd) $ concat board)
+
+part1 :: [Int] -> [Board] -> Int
+-- part1 (n:ns) boards
+--   | Just board <- find hasWon boards = calculateScore n board
+--   | otherwise = part1 ns $ map (update n) boards
+
+part1 (n:ns) boards =
+  case map (update n) boards of
+    boards'
+      | Just board <- find hasWon boards' -> n * calculateScore board
+      | otherwise -> part1 ns boards'
+
+part2 :: [Int] -> [Board] -> Int
+part2 (n:ns) boards =
+  case map (update n) boards of
+    [board] | hasWon board -> n * calculateScore board
+    boards' -> part2 ns $ filter (not . hasWon) boards'
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,9 +4,10 @@ import Control.Monad
 import qualified Day1 as D1
 import qualified Day2 as D2
 import qualified Day3 as D3
+import qualified Day4 as D4
 
 main :: IO Counts
-main = runTestTT $ TestList [day1, day2, day3]
+main = runTestTT $ TestList [day1, day2, day3, day4]
 
 withInput :: FilePath -> (String -> a) -> (a -> Assertion) -> Assertion
 withInput file parser action = action . parser =<< readFile file
@@ -65,4 +66,42 @@ day3 =
       , "11001"
       , "00010"
       , "01010"
+      ]
+
+day4 :: Test
+day4 =
+  "day 4 tests" ~:
+  TestList
+    [ "parsing" ~: (chosenNums, boards) ~=? D4.parse parseEx
+    , "part 1 example" ~: 4512 ~=? D4.part1 chosenNums boards
+    , "part 2 example" ~: 1924 ~=? D4.part2 chosenNums boards
+    , "real deal" ~: withInput "resources/4.txt" D4.parse $ \(ns, bs) -> do
+        33348 @=? D4.part1 ns bs
+        8112 @=? D4.part2 ns bs
+    ]
+  where
+    parseEx =
+      "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1\n\n22 13 17 11  0\n 8  2 23  4 24\n21  9 14 16  7\n 6 10  3 18  5\n 1 12 20 15 19\n\n 3 15  0  2 22\n 9 18 13 17  5\n19  8  7 25 23\n20 11 10 24  4\n14 21 16 12  6\n\n14 21 17 24  4\n10 16 15  9 19\n18  8 23 26 20\n22 11 13  6  5\n 2  0 12  3  7\n"
+    chosenNums =
+      [ 7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12 , 22
+      , 18, 20, 8, 19, 3, 26, 1 ]
+    boards =
+      [ [ [(22, False), (13, False), (17, False), (11, False), (0, False)]
+        , [(8, False), (2, False), (23, False), (4, False), (24, False)]
+        , [(21, False), (9, False), (14, False), (16, False), (7, False)]
+        , [(6, False), (10, False), (3, False), (18, False), (5, False)]
+        , [(1, False), (12, False), (20, False), (15, False), (19, False)]
+        ]
+      , [ [(3, False), (15, False), (0, False), (2, False), (22, False)]
+        , [(9, False), (18, False), (13, False), (17, False), (5, False)]
+        , [(19, False), (8, False), (7, False), (25, False), (23, False)]
+        , [(20, False), (11, False), (10, False), (24, False), (4, False)]
+        , [(14, False), (21, False), (16, False), (12, False), (6, False)]
+        ]
+      , [ [(14, False), (21, False), (17, False), (24, False), (4, False)]
+        , [(10, False), (16, False), (15, False), (9, False), (19, False)]
+        , [(18, False), (8, False), (23, False), (26, False), (20, False)]
+        , [(22, False), (11, False), (13, False), (6, False), (5, False)]
+        , [(2, False), (0, False), (12, False), (3, False), (7, False)]
+        ]
       ]
